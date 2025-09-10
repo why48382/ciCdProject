cca300ce24dda794a8d45675b81b6a13d4b00fbe
lee <why48382@gmail.com>
2025-09-10 10:15:48 +0900

    최초 커밋

---
+++ src/main/java/org/example/cicdproject/CiCdProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicdproject/CiCdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
